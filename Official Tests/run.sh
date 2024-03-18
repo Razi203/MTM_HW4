cc94fb0727d67f6ebe0d513be67b1d8aea73fac0
Razi203 <30razi11@gmail.com>
2024-03-18 10:06:52 +0200

    valgrind check PASSED

--- Official Tests/run.sh
+++ Official Tests/run.sh
@@ -4,6 +4,6 @@ g++ --std=c++11 -o mtmchkin -Wall -pedantic-errors -Werror -DNDEBUG *.cpp Cards/
 
 rm -f out.txt
 
-./mtmchkin "Official Tests/extra_example/deck.txt" "Official Tests/extra_example/players.txt" > out.txt
+valgrind --leak-check=full ./mtmchkin "Official Tests/extra_example/deck.txt" "Official Tests/extra_example/players.txt" > out.txt
 
 diff out.txt "Official Tests/extra_example/output.txt"
