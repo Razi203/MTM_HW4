830a0a011094f55b46d9af9a1b98afc364d45573
Razi203 <30razi11@gmail.com>
2024-03-23 14:12:14 +0200

    Added students tests

--- OfficialTests/run.sh
+++ OfficialTests/run.sh
@@ -4,6 +4,6 @@ g++ --std=c++11 -o mtmchkin -Wall -pedantic-errors -Werror -DNDEBUG *.cpp Cards/
 
 rm -f out.txt
 
-valgrind --leak-check=full ./mtmchkin "OfficialTests/invalid_cards/deck.txt" "OfficialTests/invalid_cards/players.txt" > out.txt
+valgrind --leak-check=full ./mtmchkin "OfficialTests/myTest/deck.txt" "OfficialTests/myTest/players.txt" > out.txt
 
-diff out.txt "OfficialTests/invalid_cards/output.txt"
+diff out.txt "OfficialTests/myTest/output.txt"
---
+++ test/README.md
@@ -0,0 +1,8 @@
+![Header Image](https://i.imgur.com/l9dk1ZL.png)
+# 234124-ex4-mtmchkin-test2024A
+1. First, create a `test` folder in the root directory of the project.
+2. Extract the zip file inside the `test` folder.
+3. Navigate to the `test` directory and execute the command:
+```bash
+make -f makefileMtmchkinTests
+./tester.sh
